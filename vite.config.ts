eb90117f47f3a0e67981d4a9045d0672f520dac2
ilia makarov <ilia.makarov@me.com>
2023-07-25 20:00:06 +0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import copy from "rollup-plugin-copy";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "",
+  plugins: [react()],
+  server: {
+    https: true,
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        copy({
+          hook: "writeBundle",
+          targets: [
+            { src: "./manifest.json", dest: "./dist/" },
+            { src: "./DESCRIPTION.md", dest: "./dist/" },
+            { src: "./SETUP.md", dest: "./dist/" },
+            { src: "./icon.svg", dest: "./dist/" },
+            { src: "./docs", dest: "./dist/" },
+          ],
+        }),
+      ],
+    }
+  },
+});
