4f245a837c0b7063d00b0f6b85b08bf8d014c197
OJ Abba <phavorabba@gmail.com>
2025-04-01 09:07:44 +0100

    [Feat] Create `isBitbucketAPIError` util

--- src/services/bitbucket/BitbucketError.ts
+++ src/services/bitbucket/BitbucketError.ts
@@ -1,13 +1,13 @@
 import type { BitbucketAuthError, BitbucketAPIError } from "./types";
 
 export type InitData = {
-  status: number,
-  data: BitbucketAPIError,
+  status: number;
+  data: BitbucketAPIError;
 };
 
 class BitbucketError extends Error {
   status: number;
-  data: BitbucketAuthError|BitbucketAPIError;
+  data: BitbucketAuthError | BitbucketAPIError;
 
   constructor({ status, data }: InitData) {
     const message = "Bitbucket Api Error";
@@ -18,4 +18,12 @@ class BitbucketError extends Error {
   }
 }
 
-export { BitbucketError };
+function isBitbucketAPIError(data: unknown): data is BitbucketAPIError {
+  if (!data) {
+    return false;
+  }
+
+  return data && typeof data === "object" && "error" in data;
+}
+
+export { BitbucketError, isBitbucketAPIError };
